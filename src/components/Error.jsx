cf01b6cd00320bb6aac70203af906b83235c9bad
Funbi oyede <samsonoyede@yahoo.com>
2019-05-25 19:51:54 +0100

    radium installed

---
+++ src/components/Error.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export class Error extends Component{
+
+  
+    render(){
+       return(
+           <h3>No input </h3>
+       )
+    }
+
+}
+    export default Error
